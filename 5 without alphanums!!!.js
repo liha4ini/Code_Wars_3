fd7b39cee25c3cb4a315b42839b210637936ee38
liha4ini <liha4ev.sergey@gmail.com>
2022-11-04 11:29:07 +0300

    task_1

---
+++ 5 without alphanums!!!.js
@@ -0,0 +1,8 @@
+// Write a function/proc/lambda called _ that always returns 5.
+
+// Sounds easy right? Just bear in mind that you can only use the following characters:
+
+// ~!@#$%^&*()-_+=[]\{}|:;"'<,>.?/`
+
+
+_=_=>-~-~-~-~-~[];
