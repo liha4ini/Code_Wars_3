4e730a3568ad8d35d3ee518ef8e8a30b61bc0a9b
liha4ini <liha4ev.sergey@gmail.com>
2022-11-05 11:04:49 +0300

    A crawling worm

--- загруженные/5 without alphanums!!!.js
+++
@@ -1,8 +0,0 @@
-// Write a function/proc/lambda called _ that always returns 5.
-
-// Sounds easy right? Just bear in mind that you can only use the following characters:
-
-// ~!@#$%^&*()-_+=[]\{}|:;"'<,>.?/`
-
-
-_=_=>-~-~-~-~-~[];
--- загруженные/Minimize Sum Of Array (Array Series #1).js
+++
@@ -1,6 +0,0 @@
-// Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
-
-
-function minSum(arr) {
-    return arr.sort((a, b) => a - b).reduce((prev, curr) => prev + curr * arr.pop(), 0);
-}
